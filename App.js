86bad4a709f5fc44162d40c1fd4efec2ac10b4ae
Sai Siddharth Upadhyayula <s.upadhyayula1@ufl.edu>
2021-12-27 19:08:41 -0500

    UI

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { Platform, StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Tasks';
 
 export default function App() {
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    console.log('')
+  }
   return (
     <View style={styles.container}>
       
@@ -22,11 +27,12 @@ export default function App() {
       <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Write a task'}/>
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
         <TouchableOpacity>
-
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
         </TouchableOpacity>
-        <View></View>
       </KeyboardAvoidingView>
     </View>
   );
@@ -36,8 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E8EAED'
-  },
-
+},
 tasksWrapper:{
   paddingTop:80,
   paddingHorizontal:20
@@ -49,4 +54,33 @@ sectionTitle:{
 items:{
   marginTop:30,
 },
+
+writeTaskWrapper:{
+  position:'absolute',
+  bottom:60,
+  width:'100%',
+  flexDirection:'row',
+  justifyContent:'space-around',
+  alignItems:'center'
+},
+input:{
+  paddingVertical:15,
+  paddingHorizontal:15,
+  borderRadius:60,
+  backgroundColor:'#FFF',
+  width:250,
+  borderWidth: 1,
+  borderColor:'#C0C0C0',
+},
+addWrapper:{
+  width:60,
+  height:60,
+  backgroundColor:'#FFF',
+  borderRadius: 60,
+  justifyContent:'center',
+  alignItems:'center',
+  borderWidth:1,
+  borderColor:'#C0C0C0',
+},
+addText:{},
 });
